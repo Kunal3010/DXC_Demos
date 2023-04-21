d92aae1c941f0195c38e2e48e2702584d723c28f
Lalit Kumar <kunal.lalitkumar@dxc.com>
2023-04-21 15:23:33 +0530

    updated

--- Assignment/calenderAssignment/calenderAssignment/Program.cs
+++ Assignment/calenderAssignment/calenderAssignment/Program.cs
@@ -25,7 +25,7 @@ switch (month)
         }
     case 2:
         {
-            if((year%4==0) && (year%100!=0))
+            if((year%4==0) && (year%100!=0) || (year%400==0))
             {
                 for (int i = 0; i < day; i++)
                 {
