d92e68ab07e9aca1e0f8529a182522ac652acade
Lalit Kumar <kunal.lalitkumar@dxc.com>
2023-04-21 11:31:21 +0530

    updated

---
+++ CSharp/TempConv2/TempConv2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TempConv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String ch;
+            do
+            {
+                Console.WriteLine("Enter the Temperature in Degree Celsius: ");
+                int temp = Convert.ToInt16(Console.ReadLine());
+
+                double f = temp * 1.8 + 32;
+                Console.WriteLine("Temperature(Celsius): " + temp + " Temperature(Fahrenheit): " + f);
+                Console.WriteLine("Do You Wish to Continue(y/n)?");
+                ch = Console.ReadLine();
+            } while (ch =="y");
+        }
+    }
+}
---
+++ CSharp/tempConversion/tempConversion/Program.cs
@@ -0,0 +1,7 @@
+﻿for (int i = 0; i <10; i++)
+{
+    int c = i * 10;
+    int f = ((int)((c * 1.8) + 32));
+    Console.WriteLine("Sr.No   Temperature(Celcius)   Temperature(Fahrenheit)");
+    Console.WriteLine(i+"        "+c + "                     " + f);
+}
