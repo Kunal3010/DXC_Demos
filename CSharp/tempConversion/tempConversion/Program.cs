62f976592bb383a756d6bf77c4f9a3aa034f14fb
Lalit Kumar <kunal.lalitkumar@dxc.com>
2023-04-21 11:42:49 +0530

    Updated

--- CSharp/tempConversion/tempConversion/Program.cs
+++ CSharp/tempConversion/tempConversion/Program.cs
@@ -1,7 +1,7 @@
-﻿for (int i = 0; i <10; i++)
+﻿Console.WriteLine("Sr.No   Temperature(Celcius)   Temperature(Fahrenheit)");
+for (int i = 0; i <10; i++)
 {
     int c = i * 10;
     int f = ((int)((c * 1.8) + 32));
-    Console.WriteLine("Sr.No   Temperature(Celcius)   Temperature(Fahrenheit)");
     Console.WriteLine(i+"        "+c + "                     " + f);
 }
